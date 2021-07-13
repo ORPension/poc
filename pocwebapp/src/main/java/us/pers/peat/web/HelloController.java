e54899d2ee864ad8dc1c730108f7f5bc2d66c133
Kingsley Swamidoss <KSwamidoss@Provaliant.onmicrosoft.com>
2021-07-13 10:49:17 -0400

    yy

--- pocwebapp/src/main/java/us/pers/peat/web/HelloController.java
+++ pocwebapp/src/main/java/us/pers/peat/web/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello World, Welcome to POC!";
+		return "Hello World, Welcome to POC from Shawn!";
 	} 
 
 	@RequestMapping("/home")
