8cc043d920f89427283db58da705965eb6514520
Kingsley Swamidoss <KSwamidoss@Provaliant.onmicrosoft.com>
2021-07-13 10:47:22 -0400

    comitting after merge

--- pocwebapp/src/main/java/us/pers/peat/web/HelloController.java
+++ pocwebapp/src/main/java/us/pers/peat/web/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Hello World, Welcome to POC from Shawn!";
+		return "Hello World, Welcome to POC!";
 	} 
 
 	@RequestMapping("/home")
